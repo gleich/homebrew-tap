7f016dd674da434741ce5353fbf6d949386c00af
Matthew Gleich <matthewgleich@gmail.com>
2020-05-30 20:47:52 -0400

    🚀 Update nuke

--- Formula/nuke.rb
+++ Formula/nuke.rb
@@ -1,7 +1,7 @@
 class Nuke < Formula
   desc "☢️ Force quit all applications with one terminal command on macos"
   homepage "https://mattglei.ch"
-  url "https://github.com/Matt-Gleich/nuke/releases/download/1.0.0_1/nuke"
+  url "https://github.com/Matt-Gleich/nuke/releases/download/1.0.1/nuke"
   sha256 "d249e40bc900475b834e7c82964eb760be04286c772045b8c9d024f366d72fde"
 
   def install
