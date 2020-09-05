ebab72496cc927ed4db9313161b842611ed23a4a
Matt-Gleich <matthewgleich@gmail.com>
2020-09-04 21:13:48 -0400

    Brew formula update for texsep version v2.0.2

--- Formula/texsep.rb
+++ Formula/texsep.rb
@@ -2,24 +2,24 @@
 class Texsep < Formula
   desc "Separate your pdfs from your tex files"
   homepage "https://mattglei.ch"
-  version "2.0.1"
+  version "2.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.1/texsep_2.0.1_darwin_amd64.tar.gz"
-    sha256 "2eace57deaff735a6fa0655f90f7a382784e69b76126e5a0e0d0c1713def0b71"
+    url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.2/texsep_2.0.2_darwin_amd64.tar.gz"
+    sha256 "bf35b3d563e057e12895bc82d5d448c05063a50ac5db03fa1245f72f55793a32"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.1/texsep_2.0.1_linux_amd64.tar.gz"
-      sha256 "c6a8d04b50a295b9dde48c05d9ce482e966679c4d7740b3234ff010a4a3b6270"
+      url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.2/texsep_2.0.2_linux_amd64.tar.gz"
+      sha256 "8541bed8ba291af055eadfb532d274d68655397d251649951a3632f1cea3f45c"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.1/texsep_2.0.1_linux_arm64.tar.gz"
-        sha256 "9fa42f2b241b1c843d42d1f3b5c8be37b215112b5d03c8a163d6cb0da4e862a8"
+        url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.2/texsep_2.0.2_linux_arm64.tar.gz"
+        sha256 "1c758c2ea26be3660f66eb90d4a06e80b9abdaf9bf0cd5abfaa621658522057b"
       else
-        url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.1/texsep_2.0.1_linux_armv6.tar.gz"
-        sha256 "8ffa33ffe5edaa8679ad4803ce2abdd503d5fcdce02b503eaa35a9c0af8d2b32"
+        url "https://github.com/Matt-Gleich/texsep/releases/download/v2.0.2/texsep_2.0.2_linux_armv6.tar.gz"
+        sha256 "959fa743aa10a5fce9bd649e03e21f1d9d7adb34e875b41d83956c5d51801ecb"
       end
     end
   end
